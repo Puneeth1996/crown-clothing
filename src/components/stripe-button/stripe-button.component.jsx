a1613384b7e17972177927ae6a701968b8c5f097
Puneeth P <puneeth.p@trraincircle.com>
2021-04-30 18:58:22 +0530

    Lesson-26 | styled-components to explore CSS-in-JS

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_51IltcFSBhUasS19fZliuPcSEaYQIY6v5jDCzyl4kX9nYNPR4IshbufTTIX4ta50ecNziiqluPtHdpkJm2vluWhTC00DfzuoAP5';
+  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
 
   const onToken = token => {
     console.log(token);
